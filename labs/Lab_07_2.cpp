62bc526f18030651897a894de0f644c556a797f8
Rubillex <38977402+Rubillex@users.noreply.github.com>
2019-06-01 15:37:10 +0700

    upd 7.2

--- labs/Lab_07_2.cpp
+++ labs/Lab_07_2.cpp
@@ -1,50 +1,100 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-
     char sym1, sym2;
-    string Str;
-    string StrokTemp;
-    string StrokMax;
+    string Str; //исходная строка
+   // string TempWord; //текущее слово
+    char TempWord [20];
+    string MaxWord;//максим. слово
+    MaxWord [0] = '\0';
+    char ClearStr;
+    ClearStr = ' ';
     int temp = 0;
-    int maxlen;
+    int maxlen = 0;
+    int j=0;
+    int k; // переменная для отладочной информации
     cout << "enter the string:" << '\n';
-    getline(cin, Str);
+    getline(cin, Str); //ввод строки
     sym1 = ',';
     sym2 = ' ';
-    for(size_t i=0; i<Str.size(); i++)
+    for (size_t i=0; i<Str.size(); i++) //цикл по всей строке
     {
-      if(Str[i] != sym1) //между запятыми
-      {
-          if (Str[i] != sym2) // между пробелами
-          {
-            StrokTemp.insert(StrokTemp.begin()+ i++, Str[i]);
-            //StrokMax.insert(StrokMax.begin()+ i++, Str[i]);
-            temp++;
-            //отладка
-            cout << "StrokTemp = " <<StrokTemp << endl;
-            cout << "temp = " << temp << endl;
-          }
-          else
-          {
-            if(temp>maxlen)
+        
+        
+        if (Str[i] == ',') // если запятая, то
+        {
+            
+            
+            if(temp>maxlen) // если длина текущего слова больше максимальной длины, то
             {
-              maxlen = temp;
-              StrokMax = StrokTemp;
-              StrokTemp.clear(); //очистка временной строки
-              temp = 0;
+                maxlen = temp; // обновление максимальной длины
+                MaxWord = TempWord; // обновление буфера максимальных слов
+               // cout << "MaxWord:" << MaxWord << '\n';
             }
-          }
-      }
-      else
-      {
-        cout<<StrokMax<< " ";
-        maxlen = 0;
-      }
-
+            
+            
+            cout << MaxWord << " "; // вывод текущего макс.слова
+            MaxWord.clear(); //очистка буфера максим.слов
+            memset(TempWord, 0, sizeof(TempWord)); // очистка буфера текущего слова
+            //cout << "Temp clear" << '\n';
+            temp = 0;
+            k++;
+            j=0;
+            maxlen = 0;
+        }
+        
+        
+        
+        if (Str[i] != ' ' && Str[i] != ',') // если это не пробел и не запятая, то
+        {
+            if (k>0)
+            {
+            //    cout << "Im here" << '\n';
+                k = 0;
+            }
+            temp++; // кол-во букв в слове +1
+            TempWord [j] = Str [i]; // запись символа в буфер слов
+            //cout << "TempWord: " << TempWord << '\n';
+            j++;
+        }
+        
+        
+        
+        if (Str[i] == ' ') // если это пробел, то
+        {
+            if(temp>maxlen) // если длина текущего слова больше максимальной длины, то
+            {
+                maxlen = temp; // обновление максимальной длины
+                MaxWord = TempWord; // обновление буфера максимальных слов
+               // cout << "MaxWord:" << MaxWord << '\n';
+            }
+        memset(TempWord, 0, sizeof(TempWord));
+        //cout << "Temp clear" << '\n';
+        temp = 0;
+        k++;
+        j=0;
+        }
+        
     }
+    if(temp>maxlen) // если длина текущего слова больше максимальной длины, то
+            {
+                maxlen = temp; // обновление максимальной длины
+                MaxWord = TempWord; // обновление буфера максимальных слов
+                //cout << "MaxWord:" << MaxWord << '\n';
+            }
+            
+            
+            cout << MaxWord << " "; // вывод текущего макс.слова
+            MaxWord.clear(); //очистка буфера максим.слов
+            memset(TempWord, 0, sizeof(TempWord)); // очистка буфера текущего слова
+            //cout << "Temp clear" << '\n';
+            temp = 0;
+            k++;
+            j=0;
+            maxlen = 0;
 
     return 0;
 }
