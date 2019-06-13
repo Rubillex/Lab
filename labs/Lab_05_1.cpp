58e372f56dca529f3ea99d41744b09e078467813
Rubillex <38977402+Rubillex@users.noreply.github.com>
2019-06-13 14:50:05 +0700

    Update Lab_05_1.cpp

--- labs/Lab_05_1.cpp
+++ labs/Lab_05_1.cpp
@@ -5,11 +5,33 @@ using namespace std;
  int main(){
    float arrA[25];
    float arrB[25];
+   float arrC[50];
+
    float temp;
-   cout << "Input arrA" << endl;
+
+
+   cout << "Input arr 1" << endl;
    for (int i = 0; i<25; i++)
    {
+     std::cout << "n[" << i+1 << "]= ";
      cin >> arrA[i];
+   }
+
+   cout << "Input arr 2" << endl;
+   for (int i = 0; i<25; i++)
+   {
+     std::cout << "n[" << i+1 << "]= ";
+     cin >> arrB[i];
+   }
+
+   for (int i = 0; i < 25; i++)
+   {
+     arrC[i] = arrA[i];
+   }
+
+   for (int i = 0; i < 25; i++)
+   {
+     arrC[i+25] = arrB[i];
    }
 
 
@@ -24,14 +46,48 @@ using namespace std;
 	   }
 	 }
 
+   for (int i = 0; i < 25; i++) {
+	   for (int j = 0; j < 24; j++) {
+	     if (arrB[j] > arrB[j+1]){
+	       temp = arrB[j];
+	       arrB[j] = arrB[j+1];
+	       arrB[j+1] = temp;
+	     }
+	   }
+	 }
+
+   for (int i = 0; i < 50; i++) {
+	   for (int j = 0; j < 49; j++) {
+	     if (arrC[j] > arrC[j+1]){
+	       temp = arrC[j];
+	       arrC[j] = arrC[j+1];
+	       arrC[j+1] = temp;
+	     }
+	   }
+	 }
 
 
-   cout << "array A" << endl;
+   cout << "array 1" << endl;
    for (int i = 0; i<25; i++)
    {
      cout << arrA[i] << " ";
    }
 
+   std::cout << " " << '\n';
+
+   cout << "array 2" << endl;
+   for (int i = 0; i<25; i++)
+   {
+     cout << arrB[i] << " ";
+   }
+
+   std::cout << " " << '\n';
+
+   cout << "array 3" << endl;
+   for (int i = 0; i<50; i++)
+   {
+     cout << arrC[i] << " ";
+   }
 
  return 0;
  }
