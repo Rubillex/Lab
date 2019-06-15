4a1f4d568c74865b98f931f4e38a41e24cf95d1e
Rubillex <38977402+Rubillex@users.noreply.github.com>
2019-06-15 16:19:02 +0700

    Update Lab_08_1.cpp

--- labs/Lab_08_1.cpp
+++ labs/Lab_08_1.cpp
@@ -12,30 +12,44 @@ struct info
   int quantity_c;
 };
 
-info person1 = {
-  "Ivanov",
-  "SOK",
-  176,
-  123,
-  42
-};
-
 int main()
 {
-  int sa = 40;
-  int sb = 70;
-  int sc = 47;
-
-  int summ1, summ2, summ3;
-
-  summ1 = sa * person1.quantity_a;
-  summ2 = sb * person1.quantity_b;
-  summ3 = sb * person1.quantity_c;
-
-  cout << "All cat. A: " << person1.quantity_a << "  oplata: " << summ1 <<'\n';
-  cout << "All cat. B: " << person1.quantity_b << "  oplata: " << summ2 <<'\n';
-  cout << "All cat. C: " << person1.quantity_c << "  oplata: " << summ3 <<'\n';
-
+    info person [3];
+    for (int i = 0; i < 3; i++)
+    {
+      cout << "Surname :";
+      cin >> person[i].surname;
+      cout << "Name c.:";
+      cin >> person[i].name_c;
+      cout << "quantity A:";
+      cin >> person[i].quantity_a;
+      cout << "quantity B:";
+      cin >> person[i].quantity_b;
+      cout << "quantity C:";
+      cin >> person[i].quantity_c;
+    }
+  int sa, sb, sc;
+  sa = 10;
+  sb = 19;
+  sc = 43;
+  int summ1[3], summ2[3], summ3[3], summ4[3], summ5[3];
+  
+  for (int i = 0; i < 3; i++)
+  {
+    summ1[i] = sa * person[i].quantity_a;
+    summ2[i] = sb * person[i].quantity_b;
+    summ3[i] = sc * person[i].quantity_c;
+    summ4[i] = person[i].quantity_a + person[i].quantity_b + person[i].quantity_c;
+    summ5[i] = summ1[i] + summ2[i] + summ3[i];
+    cout << "Surname:" << person[i].surname << '\n';
+    cout << "Name c:" << person[i].name_c << '\n';
+    cout << "All cat. A: " << person[i].quantity_a << "  oplata: " << summ1[i] <<'\n';
+    cout << "All cat. B: " << person[i].quantity_b << "  oplata: " << summ2[i] <<'\n';
+    cout << "All cat. C: " << person[i].quantity_c << "  oplata: " << summ3[i] <<'\n';
+    cout << "All cat.: " << summ4[i] << "  oplata: " << summ5[i] <<'\n';
+    cout << " " << '\n';
+      
+  }
 
     return 0;
 }
