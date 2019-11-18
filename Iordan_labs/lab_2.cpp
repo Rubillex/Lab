d5a9bad56b98fd8bd6c776810f0828aa8ac99ccd
Rubillex <38977402+Rubillex@users.noreply.github.com>
2019-11-18 15:49:59 +0700

    Update lab_2.cpp

--- Iordan_labs/lab_2.cpp
+++ Iordan_labs/lab_2.cpp
@@ -32,16 +32,16 @@ int main()
 	//ввод n
 	cout << "n="; // n частей разбиения
 	cin >> n;
-	N=n-1;
 
 
 
 	double *x = new double[n];
 	double *f = new double[n];
-	double *xkj = new double[n];
+	double xkj;
 
-	double q[4] = {2, 1, 0.55555, 0.65214};
-	double xj[4] = {0, 0.57735, 0.77459, 0.33998};
+
+	double q[4] = {0.3478548451, 0.6521451549, 0.6521451549, 0.3478548451};
+	double xj[4] = {-0.8611363116, -0.3399810436, 0.3399810436, 0.8611363116};
 
 
 	//шаг
@@ -50,56 +50,48 @@ int main()
 
 
 	x[0] = a; //Xk
-	for(i=1; i <= n; i++)
+	for(k=1; k <= n; k++)
 	{
-	    x[i] = x[i-1] + h;
+	    x[k] = x[k-1] + h;
 	}
 
-	for(k=0; k <= n; k++)
-	{
-	    xkj[k]=(x[k]+x[k+1])/2+xj[k]*(b-a)/(2*(n-1));
-	}
+    double sumAll = 0;
+    double sumF;
 
+    for(j=0; j< n; j++)
+    {
+        sumF = 0;
+       for(k=0; k < n; k++)
+	    {
+	       xkj=(x[k]+x[k+1])/2+xj[j]*(b-a)/(2*n);
+	       f[k]=exp(xkj);
+	       sumF+=f[k];
+	       cout << "sumF=" << sumF << endl;
+    	}
+    	sumAll += q[j] * sumF;
+    }
+    
+    
+	double result=0;
+	double Int = exp(b) - exp(a);
+	
 
+	result = (b-a)/(2*n)*sumAll;
+    
+    double ppog, tpog;
+    
+	ppog = fabs(result - Int);
 
-	for (int i = 0; i <= n; i++)
-	{
-	    f[i]=exp(xkj[i]); //составление функции
-	}
-
-	double xg;//тестовая точка
-	double result=0;//ЛАГРАНЖ
-	double pg = 0; //погрешность
-	double pog = 0;
-	double p; //ПЭ
-	double fi; //значение функции в тестовой точке
-	double temp; //temp
-	cout<<"Функция f(i)=e^x";
-
-	//начинаем считать
-
-	double sum=0;
-
-	for(j=0; j <= n; j++)
-	{
-		sum+=q[j]*f[j];
-	}
-
-	result = (b-a)/(2*(n-1))*sum;
-
-	ppog = exp(b) - exp(a);
-
-	tpog = pow(b-a, 2*n+1)/pow(N,2*n) * pow(fact(n),4)/((pow(fact(2*n)),3)*(2*n+1)) *exp;
+	tpog = pow(b-a, 2*n+1)/pow(N,2*n) * pow(fact(n),4)/((pow(fact(2*n),3)*(2*n+1))) * exp(b);
 
 
 
 	cout<<"\nИнтеграл методом Гауса:\n" << setprecision(16) <<result;
-    //	cout<<"\nФункция f(i) равна:\n" << setprecision(16) <<fi;
-	cout<<"\nПрактическая погрешность равна:\n" << ppog <<pg;
+    cout<<"\nТочное значение интеграла:\n" << setprecision(16) <<Int;
+	cout<<"\nПрактическая погрешность равна:\n" << ppog;
 	cout<<"\nТеоретическая погрешность равна:\n" << tpog;
 
     delete [] x;
     delete [] f;
-    delete [] xkj;
 	return 0;
 }
