991a46539942bba07059e30675f1ad50b3f3d47c
Rubillex <38977402+Rubillex@users.noreply.github.com>
2019-12-02 14:39:45 +0700

    Update lab_3.cpp

--- Iordan_labs/lab_3.cpp
+++ Iordan_labs/lab_3.cpp
@@ -1,6 +1,3 @@
-// Метод_Ньютона.cpp: определяет точку входа для консольного приложения
-
-// sourse http://statistica.ru/branches-maths/chislennye-metody-resheniya-uravneniy/#s3b
 #include <iostream>
 #include <math.h>
 
@@ -8,7 +5,7 @@ using namespace std;
 
 float f(double x) //возвращает значение функции f(x) = x^2-2
 {
-   return exp(x);
+   return exp(x)-1;
 }
 
 float df(float x) //возвращает значение производной
@@ -25,13 +22,15 @@ int main()
 {
    int exit = 0, i = 0;//переменные для выхода и цикла
    double x0,xn;// вычисляемые приближения для корня
-   double a, b;// границы отрезка
+   double a, b, eps;// границы отрезка
 
    do {
         i = 0;
           cout<<"Please input [a;b]\n=>";
           cin>>a>>b; // вводим границы отрезка, на котором будем искать корень
-
+            
+          cout << "\neps =";
+          cin >> eps;
           if (a > b) // если пользователь перепутал границы отрезка, меняем их местами
              {
                 x0 = a;
@@ -47,20 +46,27 @@ int main()
               else x0 = b;
                    xn = x0-f(x0)/df(x0); // считаем первое приближение
                    cout<<i++<<"-th iteration = "<<xn<<"\n";
-
-             while(exp(x) > xn) // пока не достигнем необходимой точности, будет продолжать вычислять
+            
+            
+             while(fabs(x0 - xn) > eps) // пока не достигнем необходимой точности, будет продолжать вычислять
               {
-                x0 = xn;
-                xn = x0-f(x0)/df(x0); // непосредственно формула Ньютона
+                x0 = xn; // предыдущее значение
+                xn = x0-f(x0)/df(x0); //  формула Ньютона
                 cout<<i++<<"-th iteration = "<<xn<<"\n";
               }
 
-              double ppog = exp(x) - x0;
-              double tpog = 1;
+              double ppog = 0 - xn;
+              double betta = exp(b)/2*(x0);
+              cout << "Betta =" << betta << endl;
+              
+              cout << "\n------------\n";
+              
+              double tpog = betta * (0 - x0);
 
-                cout << "\nRoot = " << x0; // вывод вычисленного корня
-                cout << "\nRoot(exp_x) = " << exp(x);
-                cout << "Pract pogr. =" << ppog;
+                cout << "\nRoot = " << xn; // вывод вычисленного корня
+                cout << "\nRoot(exp_x - 1) = " << 0;
+                cout << "\nPract pogr. =" << fabs(ppog);
+                cout << "\nTeor. pogr. =" << fabs(tpog);
           }
           cout<<"\nExit?=>";
           cin>>exit;
